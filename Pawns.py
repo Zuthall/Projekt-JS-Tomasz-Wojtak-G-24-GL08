18bf2c38d0399f19ed0cdad4e0bbca955ecf8f5f
zuthall <zuthalll@gmail.com>
2020-06-29 21:48:00 +0200

    Poprawiono wskazane błędy.

--- Pawns.py
+++ Pawns.py
@@ -1,297 +1,325 @@
-# -- STEROWANIE --
-# Aby wybrać a następnie poruszyć pionek, trzeba użyć lewego przycisku myszy.
-# !! Jeśli chcemy anulować wybór pionka należy wcisnąć prawy przycisk myszy. !!
-# Wciśnij klawisz escape aby wyłączyć program.
+""" -- STEROWANIE --
 
-# Wczytanie wymaganych bibliotek.
+Aby wybrać a następnie poruszyć pionek, trzeba użyć lewego przycisku myszy.
+!! Jeśli chcemy anulować wybór pionka należy wcisnąć prawy przycisk myszy. !!
+Wciśnij klawisz escape aby wyłączyć program."""
+
+
 import sys
-
 import time
 
 import pygame
-from pygame.locals import *
+from pygame import locals
 
-import math
 
-import assets
-import Pawns_test
-
-pygame.init()
-
-# Nazwa oraz wymiary okna.
 pygame.display.set_caption('Four Field Kono')
-length, width = 220, 220
-window_size = (length, width)
-window = pygame.display.set_mode(window_size)
+LENGTH, WIDTH = 220, 220
+WINDOW_SIZE = (LENGTH, WIDTH)
+WINDOW = pygame.display.set_mode(WINDOW_SIZE)
+MIDDLE = (75, 100)
+PLAYER1_TURN = (60, 210)
+PLAYER2_TURN = (60, 0)
+BOARD_SIZE = 4
+WAIT_2_SECONDS = 2
+LEFT_BUTTON_PRESSED = (1, 0, 0)
+RIGHT_BUTTON_PRESSED = (0, 0, 1)
+PAWN_SIZE = 50
+FRAME_WIDTH = 10
+RESET_HEIGHT = 180
+WINNER_HEIGHT = (60, 60)
+POINT_ZERO = (0, 0)
+NULL, BLACK, WHITE = range(-1, 2)
 
-# Stałe wskazujące pewne miejsca na ekranie (środek okna, nazwa gracza, itp.).
-mid = (75, 100)
-player1_turn = (60, 210)
-player2_turn = (60, 0)
 
-# Przypisanie wartosci określających gracza
-none, black, white = range(-1, 2)
+class Assets:
+    """Wczytuje grafiki."""
+
+    @staticmethod
+    def load():
+        Assets.screen = pygame.display.get_surface()
+        Assets.background = pygame.image.load('ground.png')
+        Assets.black_pawn = pygame.image.load('black.png')
+        Assets.white_pawn = pygame.image.load('white.png')
+        Assets.blank_slot = pygame.image.load('clear.png')
+        Assets.white_pawn_selected = pygame.image.load('white_selected.png')
+        Assets.black_pawn_selected = pygame.image.load('black_selected.png')
+        Assets.turn = pygame.image.load('turn.png')
+        Assets.no_turn = pygame.image.load('!turn.png')
+        Assets.win_white = pygame.image.load('win1.png')
+        Assets.win_black = pygame.image.load('win2.png')
+        Assets.wrong_move = pygame.image.load('wrong.png')
+        Assets.choose = pygame.image.load('choose.png')
 
 
-# Player_turn odpowiada turze gracza który właśnie wykonuje ruch.
-# Jeśli ktoś wygra, rozgrywka kończy się.
-# Selected = 1 pozwala wyświetlić wybrany pionek, po wykonaniu ruch oznaczenie jest usuwane.
-# Metoda change_player() kończy turę.
 class Pawns(object):
-    """Pozwala na utworzenie pionków które przechwoują informacje o tym,
-     czy są w danej chwili wybrane oraz o ich właścicielu."""
-    reset = False       # Zmienna służąca do restartowania rozgrywki.
-    player_turn = white     # Zaczynaja biale, zmiana wartosci to zmiana tury gracza.
-    winner = False
-    x, y = 0, 0
+    """ Pozwala na utworzenie pionków oraz modyfikacje ich atrybutów.
 
-    def __init__(self, player, selected):
+     Pionki przechwoują informacje o tym, czy są w danej chwili wybrane oraz o ich właścicielu.
+     Selected = True pozwala wyświetlić wybrany pionek, po wykonaniu ruch oznaczenie jest usuwane.
+     Współrzędne x oraz y pozwalają przekazać zapamiętane dane, potrzebne do wykonania np. ruchu.
+     """
+
+    reset = False
+    player_turn = WHITE
+    winner = False
+    x, y = POINT_ZERO
+
+    def __init__(self, player=NULL, selected=False):
         self.player = player
         self.selected = selected
 
     def change_player(self):
-        if self.player == black:
-            self.player = white
-        elif self.player == white:
-            self.player = black
+        if self.player == BLACK:
+            self.player = WHITE
+        elif self.player == WHITE:
+            self.player = BLACK
 
 
-# Dwuwymiaorwa plansza, na wzór:
-
-#   0 0 0 0
-#   0 0 0 0
-#   1 1 1 1
-#   1 1 1 1
-
-# Jedynki oznaczają pionki gracza drugiego, natomiast zera, gracza pierwszego.
-# Jedynki oraz zera są zapisane są w komórkach 'player'.
 def create_board():
-    return [[Pawns(black, False) if i < 2 else Pawns(white, False) for i in range(0, 4)] for j in range(0, 4)]
+    """" Funkcja pozwala na utworzenie planszy.
+
+    Dwuwymiaorwa plansza, na wzór, gdzie 1 to białe, natomiast 0 to czarne.:
+
+   (0,0)      (3,0)
+
+      0  0  0  0
+      0  0  0  0
+      1  1  1  1
+      1  1  1  1
+
+   (0,3)      (3,3)
+
+  """
+    return [[Pawns(BLACK, False) if i < BOARD_SIZE/2 else Pawns(WHITE, False)
+             for i in range(BOARD_SIZE)] for j in range(BOARD_SIZE)]
 
 
-# Funkcja tworzy plansze wypełnioną pionkami.
 board = create_board()
 
 
-# Funkcja change_player() kończy bieżącą turę
 def change_players_turn():
-    if Pawns.player_turn == white:
-        Pawns.player_turn = black
-    elif Pawns.player_turn == black:
-        Pawns.player_turn = white
+    """Funkcja kończy bieżącą ture."""
+    if Pawns.player_turn == WHITE:
+        Pawns.player_turn = BLACK
+    elif Pawns.player_turn == BLACK:
+        Pawns.player_turn = WHITE
 
 
-# Funkcja sprawdza pozycje myszki podczas wciskania lewgo przycisku myszy i
-# przypisuje konkretne wartości odpowaidające romiezczeniu pionków
-# do zmiennych statycznych klasy Pawns: Pawns.x oraz Pawns.y
-# Sprawdzając, czy współrzędne nie wychodzą poza obszar planszy.
-def pos():
+def get_position():
+    """Funkcja sprawdza pozycje myszki.
+
+    Przypisuje konkretne wartości odpowaidające romiezczeniu pionków
+    do zmiennych statycznych klasy Pawns: Pawns.x oraz Pawns.y
+    Sprawdzając, czy wybrane współrzędne nie wychodzą poza obszar planszy."""
     x, y = pygame.mouse.get_pos()
-    if 210 > x > 180 and 220 > y > 210:
+    if LENGTH > y > LENGTH - FRAME_WIDTH > x > RESET_HEIGHT:
         Pawns.reset = True
     reset()
-    while not (length > x >= 10 and width > y >= 10):
+    while not (LENGTH - FRAME_WIDTH > x >= FRAME_WIDTH and WIDTH - FRAME_WIDTH > y >= FRAME_WIDTH):
         pygame.event.get()
-        if pygame.mouse.get_pressed() == (1, 0, 0):
+        if pygame.mouse.get_pressed() == LEFT_BUTTON_PRESSED:
             x, y = pygame.mouse.get_pos()
-    x, y = (x - 10) // 55, (y - 10) // 55
+    x, y = (x - FRAME_WIDTH) // PAWN_SIZE, (y - FRAME_WIDTH) // PAWN_SIZE
     Pawns.x, Pawns.y = x, y
 
 
-# Funkcja sprawdza, czy ktoryś z graczy wygrał, jeśli tak, ustawia
-# wartość Pawns.winner na True i wyświetla odpowiedni obrazek.
 def check_winner():
-    a, b = 0, 0
-    for i in range(0, 4):
-        for j in range(0, 4):
-            if board[i][j].player == white:
-                a += 1
-            elif board[i][j].player == black:
-                b += 1
-    if a == 1:
-        assets.Assets.screen.blit(assets.Assets.win_white, (60, 60))
+    """Funkcja sprawdza czy rozgrywka nadal trwa, jeśli nie, wyświetla odpowiedni komunikat."""
+    player1_pawns, player2_pawns = POINT_ZERO
+    for i in range(BOARD_SIZE):
+        for j in range(BOARD_SIZE):
+            if board[i][j].player == WHITE:
+                player1_pawns += 1
+            elif board[i][j].player == BLACK:
+                player2_pawns += 1
+    if player1_pawns == 1:
+        Assets.screen.blit(Assets.win_WHITE, WINNER_HEIGHT)
         pygame.display.flip()
-        time.sleep(2)
+        time.sleep(WAIT_2_SECONDS)
         Pawns.winner = True
-    elif b == 1:
-        assets.Assets.screen.blit(assets.Assets.win_black, (60, 60))
+    elif player2_pawns == 1:
+        Assets.screen.blit(Assets.win_black, WINNER_HEIGHT)
         pygame.display.flip()
-        time.sleep(2)
+        time.sleep(WAIT_2_SECONDS)
         Pawns.winner = True
 
 
-# Wyświetla i odświeża tło.
 def menu():
-    assets.Assets.screen.blit(assets.Assets.background, (0, 0))
+    """Wyświetla i odświeża tło."""
+    Assets.screen.blit(Assets.background, POINT_ZERO)
     pygame.display.flip()
 
 
-# Sprawdza zmienne odpowiadające wyświetlaniu obrazków,
-# jeśli dane są prawdziwe, wyświetla je w odpowiednim miejscu.
 def refresh():
-    for i in range(0, 4):
-        place1 = 10 + i * 50
-        for j in range(0, 4):
-            place2 = 10 + j * 50
-            if board[i][j].player == black:
-                assets.Assets.screen.blit(assets.Assets.black_pawn, (place1, place2))
-                if board[i][j].selected == 1:
-                    assets.Assets.screen.blit(assets.Assets.black_pawn_selected, (place1, place2))
-            elif board[i][j].player == white:
-                assets.Assets.screen.blit(assets.Assets.white_pawn, (place1, place2))
-                if board[i][j].selected == 1:
-                    assets.Assets.screen.blit(assets.Assets.white_pawn_selected, (place1, place2))
-            elif board[i][j].player == none:
-                assets.Assets.screen.blit(assets.Assets.no_turn, player1_turn)
-                assets.Assets.screen.blit(assets.Assets.blank_slot, (place1, place2))
-                assets.Assets.screen.blit(assets.Assets.no_turn, player2_turn)
-            if Pawns.player_turn == black:
-                assets.Assets.screen.blit(assets.Assets.no_turn, player1_turn)
-                assets.Assets.screen.blit(assets.Assets.turn, player2_turn)
-            elif Pawns.player_turn == white:
-                assets.Assets.screen.blit(assets.Assets.no_turn, player2_turn)
-                assets.Assets.screen.blit(assets.Assets.turn, player1_turn)
+    """Odpowiada za wyświetlnaie grafik."""
+    for i in range(BOARD_SIZE):
+        place1 = FRAME_WIDTH + i * PAWN_SIZE
+        for j in range(BOARD_SIZE):
+            place2 = FRAME_WIDTH + j * PAWN_SIZE
+            if board[i][j].player == BLACK:
+                Assets.screen.blit(Assets.black_pawn, (place1, place2))
+                if board[i][j].selected:
+                    Assets.screen.blit(Assets.black_pawn_selected, (place1, place2))
+            elif board[i][j].player == WHITE:
+                Assets.screen.blit(Assets.white_pawn, (place1, place2))
+                if board[i][j].selected:
+                    Assets.screen.blit(Assets.white_pawn_selected, (place1, place2))
+            elif board[i][j].player == NULL:
+                Assets.screen.blit(Assets.no_turn, PLAYER1_TURN)
+                Assets.screen.blit(Assets.blank_slot, (place1, place2))
+                Assets.screen.blit(Assets.no_turn, PLAYER2_TURN)
+            if Pawns.player_turn == BLACK:
+                Assets.screen.blit(Assets.no_turn, PLAYER1_TURN)
+                Assets.screen.blit(Assets.turn, PLAYER2_TURN)
+            elif Pawns.player_turn == WHITE:
+                Assets.screen.blit(Assets.no_turn, PLAYER2_TURN)
+                Assets.screen.blit(Assets.turn, PLAYER1_TURN)
     pygame.display.flip()
     pygame.event.get()
 
 
-# Sprawdza czy ruch może się odbyć.
-# Jeśli użytkownik kliknie poza planszę lub wybierze własny pionek to funkcja zwróci False.
 def can_move1():
+    """Sprawdza czy ruch może się odbyć."""
     if board[Pawns.x][Pawns.y].player != Pawns.player_turn:
-        if (4 > (Pawns.x + i) >= 0 and 4 > (Pawns.y + i) >= 0 for i in [-1, 1]):
+        if (BOARD_SIZE > (Pawns.x + i) >= 0 and BOARD_SIZE > (Pawns.y + i) >= 0 for i in [-1, 1]):
             return True
     return False
 
 
-# Kolejne warunki, sprawdzające czy ruch może się odbyć.
 def can_move2(x0, y0):
+    """Sprawdza czy ruch może się odbyć. Kolejne warunki."""
     x1, y1 = Pawns.x, Pawns.y
-    if (math.fabs(x1 - x0) == 1 and math.fabs(y1 - y0) == 0) \
-            or (math.fabs(y1 - y0) == 1 and math.fabs(x1 - x0) == 0):
-        if board[x1][y1].player == none:
+    if abs(x1 - x0) + abs(y1 - y0) == 1:
+        if board[x1][y1].player == NULL:
             return True
     else:
         return False
 
 
-# Sprawdza czy wybór pionka może się odbyć.
 def can_select():
+    """Sprawdza czy wybór pionka może się odbyć."""
     if (4 > (Pawns.x + i) >= 0 and 4 > (Pawns.y + i) >= 0 for i in [-1, 1]):
         if board[Pawns.x][Pawns.y].player == Pawns.player_turn:
-            print(Pawns.x, Pawns.y)
             return True
         else:
-            assets.Assets.screen.blit(assets.Assets.wrong_move, mid)
+            Assets.screen.blit(Assets.wrong_move, MIDDLE)
             pygame.display.flip()
     return False
 
 
-# Zaznacza pionek.
 def select(x, y):
-    board[x][y].selected = 1
+    board[x][y].selected = True
     refresh()
 
 
-# Odznacza pionek
 def deselect(x, y):
-    board[x][y].selected = 0
+    board[x][y].selected = False
 
 
-# Funkcja sprawdzająca czy można przejąc pionek w określonej sytuacji.
 def can_capture(x0, y0):
+    """Funkcja sprawdzająca czy można przejąc pionek w określonej sytuacji."""
     x1, y1 = Pawns.x, Pawns.y
     if (board[(x1 + x0) // 2][(y1 + y0) // 2].player == Pawns.player_turn) and\
-            ((math.fabs(x1 - x0) == 2 and math.fabs(y1 - y0) == 0)
-             or (math.fabs(y1 - y0) == 2 and math.fabs(x1 - x0) == 0))\
-            and board[Pawns.x][Pawns.y].player != none:
+            ((abs(x1 - x0) == 2 and abs(y1 - y0) == 0)
+             or (abs(y1 - y0) == 2 and abs(x1 - x0) == 0))\
+            and board[Pawns.x][Pawns.y].player != NULL:
         return True
+
     else:
         pygame.display.flip()
-        assets.Assets.screen.blit(assets.Assets.wrong_move, mid)
-        return False
+        Assets.screen.blit(Assets.wrong_move, MIDDLE)
+    return False
 
 
-# Jeśli wszystkie wymagania do wykonania ruchu/przejęcia są spełnione to
-# zostanie wykonany, odpowiednio, ruch bądź przejęcie.
-def capture_or_move():
+def capture_or_move(go_to=(-1, -1)):
+    """Odpowiada za przemieszczanie się pionków.
+
+    Jeśli wszystkie wymagania do wykonania ruchu/przejęcia są spełnione to
+    zostanie wykonany, odpowiednio, ruch bądź przejęcie."""
 
     x, y = Pawns.x, Pawns.y
     deselect(x, y)
-    while not (can_move1() and (can_capture(x, y) or can_move2(x, y))):
-        pygame.event.get()
-        if pygame.mouse.get_pressed() == (1, 0, 0):
-            pos()
-            if board[Pawns.x][Pawns.y].player == Pawns.player_turn and (Pawns.x != x and Pawns.y != y) and reset():
-                pygame.display.flip()
-                assets.Assets.screen.blit(assets.Assets.wrong_move, mid)
-        if pygame.mouse.get_pressed() == (0, 0, 1):
-            deselect(x, y)
-            refresh()
-            return False
-    if board[Pawns.x][Pawns.y].player == Pawns.player_turn and (Pawns.x != x and Pawns.y != y) and reset():
-        pygame.display.flip()
-        assets.Assets.screen.blit(assets.Assets.wrong_move, mid)
-    board[Pawns.x][Pawns.y].player = Pawns.player_turn
-    board[x][y].player = none
-    change_players_turn()
-    refresh()
+    if go_to == (-1, -1):
+        while not (can_move1() and (can_capture(x, y) or can_move2(x, y))):
+            pygame.event.get()
+            if pygame.mouse.get_pressed() == LEFT_BUTTON_PRESSED:
+                get_position()
+                if board[Pawns.x][Pawns.y].player == Pawns.player_turn and (Pawns.x != x and Pawns.y != y) and reset():
+                    pygame.display.flip()
+                    Assets.screen.blit(Assets.wrong_move, MIDDLE)
+            if pygame.mouse.get_pressed() == RIGHT_BUTTON_PRESSED:
+                deselect(x, y)
+                refresh()
+                return False
+        if board[Pawns.x][Pawns.y].player == Pawns.player_turn and (Pawns.x != x and Pawns.y != y) and reset():
+            pygame.display.flip()
+            Assets.screen.blit(Assets.wrong_move, MIDDLE)
+        board[Pawns.x][Pawns.y].player = Pawns.player_turn
+        board[x][y].player = NULL
+        change_players_turn()
+        refresh()
+    else:
+        Pawns.x, Pawns.y = go_to
+        if board[Pawns.x][Pawns.y] != Pawns.player_turn:
+            board[Pawns.x][Pawns.y].player = Pawns.player_turn
+            board[x][y].player = NULL
+            change_players_turn()
     return True
 
 
 def choose():
-    """Funkcja pozwalająca wybrać gracza zaczynającego rozgrywkę."""
-    assets.Assets.screen.blit(assets.Assets.choose, (0, 0))
+    """Funkcja pozwalająca, w menu, wybrać gracza zaczynającego rozgrywkę."""
+    Assets.screen.blit(Assets.choose, POINT_ZERO)
     pygame.display.flip()
-    Pawns.player_turn = none
-    while Pawns.player_turn == none:
+    Pawns.player_turn = NULL
+    while Pawns.player_turn == NULL:
         events = pygame.event.get()
         for event in events:
-            if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == K_ESCAPE):
+            if event.type == pygame.locals.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.locals.K_ESCAPE):
                 sys.exit(0)
-            elif pygame.mouse.get_pressed() == (1, 0, 0):
+            elif pygame.mouse.get_pressed() == LEFT_BUTTON_PRESSED:
                 x, y = pygame.mouse.get_pos()
-                if 200 > x > 20 and 185 > y > 100:
-                    if 105 > x > 20:
-                        Pawns.player_turn = black
+                if 200 > x > 2*FRAME_WIDTH and 185 > y > 100:
+                    if 105 > x > 2*FRAME_WIDTH:
+                        Pawns.player_turn = BLACK
                     elif 200 > x > 115:
-                        Pawns.player_turn = white
+                        Pawns.player_turn = WHITE
 
 
 def reset():
-    """Funkcja ta pozwala resetować rozgrywkę podczas jej trwania - wystarczy kliknąc w pole 'reset'"""
+    """Funkcja ta pozwala resetować rozgrywkę podczas jej trwania - należy kliknąć w pole 'reset'. """
     if Pawns.reset:
         Pawns.reset = False
-        Pawns.player_turn = none
-        for i in range(0, 4):
-            for j in range(0, 4):
+        Pawns.player_turn = NULL
+        for i in range(BOARD_SIZE):
+            for j in range(BOARD_SIZE):
                 board[j][i].selected = False
                 if i < 2:
-                    board[j][i].player = black
+                    board[j][i].player = BLACK
                 else:
-                    board[j][i].player = white
+                    board[j][i].player = WHITE
         choose()
         menu()
         refresh()
     return False
 
 
-# Odpowiada za poprawne uruchomienie i działanie programu.
 def main():
-    # Wczytuje obrazy a następnie je wyświetla
-    assets.Assets.load()
+    """Odpowiada za poprawne uruchomienie oraz działanie programu."""
+    pygame.init()
+    Assets.load()
     choose()
     menu()
     refresh()
 
-    # Włącza grę i odpowiednio reaguje na napotkane wydarzenia.
     while not Pawns.winner:
         events = pygame.event.get()
         for event in events:
-            if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == K_ESCAPE):
+            if event.type == pygame.locals.QUIT or (event.type == pygame.KEYDOWN
+                                                    and event.key == pygame.locals.K_ESCAPE):
                 sys.exit(0)
-            elif pygame.mouse.get_pressed() == (1, 0, 0):
-                pos()
+            elif pygame.mouse.get_pressed() == LEFT_BUTTON_PRESSED:
+                get_position()
                 if can_select():
                     select(Pawns.x, Pawns.y)
                     if not capture_or_move():
@@ -301,6 +329,5 @@ def main():
 
 if __name__ == '__main__':
 
-    # Wywołanie głównej funkcji.
     main()
 
--- Pawns_test.py
+++ Pawns_test.py
@@ -1,40 +1,50 @@
 import unittest
+
 import Pawns as Pa
-import assets
-
-# wizualizacja planszy, gdzie 1 - białe, 0 - czarne. Zaczynają białe.
-
-# (0,0)     (3,0)
-#   0  0  0  0
-#   0  0  0  0
-#   1  1  1  1
-#   1  1  1  1
-# (0,3)     (3,3)
 
 
 class PawnsTest(unittest.TestCase):
     """Klasa odpowiadająca za testy."""
 
-    # Test sprawdzający, czy można przejąć inny pionek.
     def test_can_capture(self):
         Pa.Pawns.x, Pa.Pawns.y = 1, 3
-        x, y = 1, 1
-        self.assertTrue(Pa.can_capture(x, y))
+        self.assertTrue(Pa.can_capture(1, 1))
 
     def test_winner(self):
         self.assertFalse(Pa.Pawns.winner)
 
-    # Test sprawdzający, czy można wykonać ruch.
-    def test_can_move(self):
-        x, y = 0, 1
-        Pa.board[x][y].player = Pa.none
+    def test_can_move1(self):
+        Pa.board[0][1].player = Pa.NULL
         Pa.Pawns.x, Pa.Pawns.y = 0, 2
-        self.assertFalse(Pa.can_move1() and Pa.can_move2(x, y))
+        self.assertTrue(Pa.can_move1)
 
-    # Test sprawdzający, czy można wybrać biały pionek.
+    def test_can_move2(self):
+        Pa.board[0][1].player = Pa.NULL
+        Pa.Pawns.x, Pa.Pawns.y = 0, 2
+        self.assertFalse(Pa.can_move2(0, 1))
+
     def test_can_select(self):
-        Pa.Pawns.x, Pa.Pawns.y = 0, 3    # lewy dolny pionek.
+        Pa.Pawns.x, Pa.Pawns.y = 0, 3
         self.assertTrue(Pa.can_select())
+
+    def test_capture(self):
+        Pa.Pawns.x, Pa.Pawns.y = 1, 3
+        Pa.capture_or_move((1, 1))
+        self.assertEqual(Pa.WHITE, Pa.board[1][1].player)
+
+    def test_move(self):
+        Pa.Pawns.x, Pa.Pawns.y = 1, 1
+        Pa.board[1][0].player = Pa.NULL
+        Pa.capture_or_move((1, 0))
+        self.assertEqual(Pa.BLACK, Pa.board[1][0].player)
+
+    def test_can_not_move1(self):
+        Pa.Pawns.x, Pa.Pawns.y = 1, 3
+        self.assertFalse(Pa.can_move1())
+
+    def test_can_not_move2(self):
+        Pa.Pawns.x, Pa.Pawns.y = 2, 3
+        self.assertFalse(Pa.can_move2(-1, 10))
 
 
 if __name__ == '__main__':
