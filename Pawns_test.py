6d2e8fa19452caa7615901d103fc15b2c66fb627
zuthall <zuthalll@gmail.com>
2020-07-04 15:23:33 +0200

    .

--- Pawns_test.py
+++ Pawns_test.py
@@ -2,49 +2,58 @@ import unittest
 
 import Pawns as Pa
 
+board = Pa.create_board()
+
+do_reset = False
+winner = False
+save = False
+
+remembered_x, remembered_y = Pa.POINT_ZERO
+player_turn = Pa.WHITE
+
 
 class PawnsTest(unittest.TestCase):
     """Klasa odpowiadająca za testy."""
 
     def test_can_capture(self):
-        Pa.Pawns.x, Pa.Pawns.y = 1, 3
-        self.assertTrue(Pa.can_capture(1, 1))
+        remembered_x, remembered_y = 1, 3
+        self.assertTrue(Pa.can_capture(1, 1, board, remembered_x, remembered_y, player_turn))
 
     def test_winner(self):
-        self.assertFalse(Pa.Pawns.winner)
+        self.assertFalse(winner)
 
     def test_can_move1(self):
-        Pa.board[0][1].player = Pa.NULL
-        Pa.Pawns.x, Pa.Pawns.y = 0, 2
-        self.assertTrue(Pa.can_move1)
+        board[0][1].player = Pa.NULL
+        remembered_x, remembered_y = 0, 2
+        self.assertFalse(Pa.can_move1(board, remembered_x, remembered_y, player_turn))
 
     def test_can_move2(self):
-        Pa.board[0][1].player = Pa.NULL
-        Pa.Pawns.x, Pa.Pawns.y = 0, 2
-        self.assertFalse(Pa.can_move2(0, 1))
+        board[0][1].player = Pa.NULL
+        Pa.remembered_x,remembered_y = 0, 2
+        self.assertFalse(Pa.can_move2(0, 1, board, remembered_x, remembered_y))
 
     def test_can_select(self):
-        Pa.Pawns.x, Pa.Pawns.y = 0, 3
-        self.assertTrue(Pa.can_select())
+        remembered_x, remembered_y = 0, 3
+        self.assertTrue(Pa.can_select(board, remembered_x, remembered_y, player_turn))
 
     def test_capture(self):
-        Pa.Pawns.x, Pa.Pawns.y = 1, 3
-        Pa.capture_or_move((1, 1))
-        self.assertEqual(Pa.WHITE, Pa.board[1][1].player)
+        remembered_x, remembered_y = 1, 3
+        Pa.capture_or_move(board, do_reset, remembered_x, remembered_y, player_turn, (1, 1))
+        self.assertEqual(Pa.WHITE, board[1][1].player)
 
     def test_move(self):
-        Pa.Pawns.x, Pa.Pawns.y = 1, 1
-        Pa.board[1][0].player = Pa.NULL
-        Pa.capture_or_move((1, 0))
-        self.assertEqual(Pa.BLACK, Pa.board[1][0].player)
+        remembered_x, remembered_y = 1, 1
+        board[1][0].player = Pa.NULL
+        Pa.capture_or_move(board, do_reset, remembered_x, remembered_y, player_turn, (1, 0))
+        self.assertEqual(Pa.WHITE, board[1][0].player)
 
     def test_can_not_move1(self):
-        Pa.Pawns.x, Pa.Pawns.y = 1, 3
-        self.assertFalse(Pa.can_move1())
+        remembered_x, remembered_y = 1, 3
+        self.assertFalse(Pa.can_move1(board, remembered_x, remembered_y, player_turn))
 
     def test_can_not_move2(self):
-        Pa.Pawns.x, Pa.Pawns.y = 2, 3
-        self.assertFalse(Pa.can_move2(-1, 10))
+        remembered_x, remembered_y = 2, 3
+        self.assertFalse(Pa.can_move2(-1, 10, board, remembered_x, remembered_y))
 
 
 if __name__ == '__main__':
